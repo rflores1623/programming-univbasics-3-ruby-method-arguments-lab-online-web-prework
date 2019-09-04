f2075214f9497eac3a7ae5f391dabe68ed17cf1a
Roberta Flores <r.flores.1227@gmail.com>
2019-09-04 04:51:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,12 @@ introduction("Josh")
 introduction("Dan")
 
 
+def introduction_with_language(name, language)
+	puts "Hi my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language("Josh", "Ember.js")
+introduction_with_language("Dan", "React")
 
 
 
